2dd4a0244cb5141bc79fd9b77aa571b6b7729795
nabiyounibuzzsolutions <iman.nabiyouni@buzzsolutions.co>
2021-04-12 19:03:31 -0700

    adding logs

--- yolov5_object_detector.py
+++ yolov5_object_detector.py
@@ -17,19 +17,23 @@ app = Flask(__name__)
 def object_detector():
 	# reading passed arguments as a json variable
 	image_path = request.get_json(Force=True)
+	current_app.logger.info("\nObject detection for the image {}\n".format(image_path))
 	# craeting aclient for uploading images to the GCP storage
 	storage_client = storage.Client.from_service_account_json(GOOGLE_APPLICATION_CREDENTIALS)
 	bucket = storage_client.bucket(bucket_name)
 	blob = bucket.blob(image_path)
 	# downloading the image from the GCP bucket
+	current_app.logger.info("\nReading the image from the bucket ...")
 	blob.download_to_filename(download_path)
 	img = cv2.imread(download_path)  # BGR
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # reading the model weights
+	current_app.logger.info("\nLoading the object detector ...")
 	model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model = model_weights)
 	model.conf = 0.01 # confidence threshold
 	model.iou = 0.6 # iou threshold
     # passing the image to the model for prediction
+	current_app.logger.info("\nDetecting objects of the image...")
 	results = model(img).xyxy[0].numpy()
 	# returning the detected objects info to the user
 	return results
